8f843b48cac6117036b95e70e15ca4b03c9945bc
Justin.Lee <madoka2000@gmail.com>
2024-07-13 01:15:25 +0800

    change password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -258,7 +258,8 @@ c.NotebookApp.open_browser = False
 #    from notebook.auth import passwd; passwd()
 #  
 #  The string should be of the form type:salt:hashed-password.
-c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$gLEClv04Sv/PsgMbFb/3yA$w6KIGq3maHtfwh+07/gmrg'
+# c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$gLEClv04Sv/PsgMbFb/3yA$w6KIGq3maHtfwh+07/gmrg'
+c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$O0l/sTuEYZbLrGx4cKmEMA$hr/xoIhYRjAmGcnKHMonoptBEZn/fvScbCGvZU84RC0'
 
 ## Forces users to use a password for the Notebook server. This is useful in a
 #  multi user environment, for instance when everybody in the LAN can access each
